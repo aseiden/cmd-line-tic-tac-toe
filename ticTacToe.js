c82ababe15a66b70c6597108ba03cd878dd97ed5
Alex Seiden <alex.seiden@gmail.com>
2017-06-19 15:02:45 -0700

    (feat) check for valid move

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,6 +3,7 @@ const prompt = require('prompt');
 class Game {
   constructor() {
     this.board = [['', '', ''], ['', '', ''], ['', '', '']];
+    this.validRowsAndColumns = {0: 0, 1: 1, 2: 2};
   }
 
   printBoard() {
@@ -17,9 +18,19 @@ class Game {
       if (err) {
         console.log('error: ', err);
       } else {
-        console.log(move);
+        if (this.isValidMove(move)) {
+          console.log('valid move');
+        }
       }
     });
+  }
+
+  isValidMove(move) {
+    if ((move.row in this.validRowsAndColumns) && (move.column in this.validRowsAndColumns) && this.board[move.row][move.column] !== ' ') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
