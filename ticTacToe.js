0286524a519ddec8ce329a285b0841cc1983c896
Alex Seiden <alex.seiden@gmail.com>
2017-06-19 14:50:58 -0700

    (feat) make basic game class and print board method

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,16 @@
-window.prompt('this is a test');
+const prompt = require('prompt');
+
+class Game {
+  constructor() {
+    this.board = [['', '', ''], ['', '', ''], ['', '', '']];
+  }
+
+  printBoard() {
+    this.board.forEach((row) => {
+      console.log(row);
+    });
+  }
+}
+
+const game = new Game();
+game.printBoard();
